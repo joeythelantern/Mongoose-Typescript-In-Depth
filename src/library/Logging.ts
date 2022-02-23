8e297e97be47f9543a660d2f43a61892526be91e
Saman <15827947+joeythelantern@users.noreply.github.com>
2022-02-22 20:27:00 -0500

    Logging fix

--- src/library/Logging.ts
+++ src/library/Logging.ts
@@ -1,7 +1,8 @@
 import chalk from 'chalk';
 
 export default class Logging {
-    public static info = (args: any) => console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO]`), chalk.blueBright(args));
-    public static warning = (args: any) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [WARN]`), chalk.yellowBright(args));
-    public static error = (args: any) => console.log(chalk.red(`[${new Date().toLocaleString()}] [ERROR]`), chalk.redBright(args));
+    public static log = (args: any) => this.info(args);
+    public static info = (args: any) => console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO]`), typeof args === 'string' ? chalk.blueBright(args) : args);
+    public static warning = (args: any) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [WARN]`), typeof args === 'string' ? chalk.yellowBright(args) : args);
+    public static error = (args: any) => console.log(chalk.red(`[${new Date().toLocaleString()}] [ERROR]`), typeof args === 'string' ? chalk.redBright(args) : args);
 }
